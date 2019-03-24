03cfabb867d46cb13c8f5a30a425a94d7f31384b
JCaw <40725916+jcaw@users.noreply.github.com>
2019-03-24 15:56:38 +0000

    Remove option to extract all colors from fonts
    Using the Ansi colors is much more consistent than extracting all colors from
    fonts. The font extraction method was unreliable and messy, it clutters up the
    package. Remove it.

--- theme-magic.el
+++ theme-magic.el
@@ -48,20 +48,6 @@
 (require 'ansi-color)
 
 
-(defvar theme-magic-theming-method 'ansi
-  "How to extract terminal colours from Emacs.
-
-Possible values are `ansi' and `fonts'.
-
-`ansi'  - Use the ansi colors defined in
-          `ansi-color-names-vector'. This is the preferred
-          method, but it will fail if the theme failed to define
-          ansi colors.
-
-`fonts' - Try to extract a nice color palette from inbuilt fonts.
-          Legacy method.")
-
-
 (defvar theme-magic--theming-functions
   '(
     load-theme
@@ -74,36 +60,6 @@ Possible values are `ansi' and `fonts'.
     )
   "Functions that should trigger an update of the linux theme.
 
-
-
-(defvar theme-magic--preferred-fonts
-  '(
-    ;; Color0 uses the background of the default face so we don't include it.
-    nil                                 ; Color0
-    font-lock-string-face               ; Color1
-    font-lock-doc-face                  ; Color2
-    font-lock-keyword-face              ; Color3
-    font-lock-preprocessor-face         ; Color4 - is this right?
-    error                               ; Color5
-    font-lock-function-name-face        ; Color6
-    default                             ; Color7 - foreground
-    ;; Colours 8-15 are alternate colours. Normally this seems to mean they are
-    ;; "pale versions" of the main 8 colours, except colour 8, which is halfway
-    ;; between the background and foreground. Pywal doesn't seem to use pale
-    ;; colours here - it just adds a shadow for color 8, then defers back to the
-    ;; first 8 colours.
-    shadow                              ; Color8 - pale background.
-    ;; Defer to normal colours for colors 9-15.
-    font-lock-string-face               ; Color9
-    font-lock-doc-face                  ; Color10
-    font-lock-keyword-face              ; Color11
-    font-lock-preprocessor-face         ; Color12
-    error                               ; Color13
-    font-lock-function-name-face        ; Color14
-    default                             ; Color15 - foreground pale
-    )
-  ;; TODO: Docstring
-  "List of fonts to extract the 16 terminal colours from.")
 \(Iff auto-updating is enabled.\)")
 
 
@@ -155,24 +111,6 @@ E.g. \"Orange\" -> \"#FFA500\"."
   "Extract the foreground color of the default face, in hex."
   (theme-magic--color-name-to-hex
    (face-foreground 'default)))
-
-
-(defun theme-magic--extract-font-colors ()
-  "Extract 16 terminal colours from the inbuilt fonts.
-
-This is designed to give a wide range of colors set by the
-current theme."
-  ;; TODO: When duplicate colors are found, iterate over fallback fonts.
-
-  ;; First colour is just the background color of the default face.
-  (mapcar 'theme-magic--color-name-to-hex
-          (cons
-           (theme-magic--extract-background-color)
-           (mapcar (lambda (font)
-                     (face-foreground font))
-                   ;; Ignore the first font - it should be nil because we get
-                   ;; the background color a different way.
-                   (cdr theme-magic--preferred-fonts)))))
 
 
 (defun theme-magic--check-dependencies ()
@@ -255,22 +193,6 @@ handling."
     (seq-into ansi-colors-vector 'list)))
 
 
-(defun theme-magic-from-emacs-ansi ()
-  "Theme Linux based on the Emacs theme, using `ansi-color-names-vector'."
-  (interactive)
-  (theme-magic--check-dependencies)
-  (theme-magic--apply-colors-with-pywal
-   (theme-magic--16-colors-from-ansi)))
-
-
-(defun theme-magic-from-emacs-fonts ()
-  "Theme Linux based on the Emacs theme, extracting colors from fonts."
-  (interactive)
-  (theme-magic--check-dependencies)
-  (theme-magic--apply-colors-with-pywal
-   (theme-magic--extract-font-colors)))
-
-
 ;;;###autoload
 (defun theme-magic-from-emacs ()
   "Theme the rest of Linux based on the Emacs theme."
@@ -278,12 +200,8 @@ handling."
   ;; This will actually check dependencies twice, but that's fine - it's cheap
   ;; we want to do it up front.
   (theme-magic--check-dependencies)
-  (cond ((eq theme-magic-theming-method 'ansi)
-         (theme-magic-from-emacs-ansi))
-        ((eq theme-magic-theming-method 'fonts)
-         (theme-magic-from-emacs-fonts))
-        (t (user-error (format "Unknown theming method: '%s'"
-                               theme-magic-theming-method)))))
+  (theme-magic--apply-colors-with-pywal
+   (theme-magic--16-colors-from-ansi)))
 
 
 (defun theme-magic-from-emacs--wrapper (&rest _)
