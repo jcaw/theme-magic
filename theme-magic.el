c7ae3571f30f70660c6f7d385617032fedf82a80
jcaw <40725916+jcaw@users.noreply.github.com>
2019-04-27 13:54:52 -0700

    Bump version to 0.2.1

--- theme-magic.el
+++ theme-magic.el
@@ -5,7 +5,7 @@
 ;; Author: GitHub user "jcaw" <40725916+jcaw@users.noreply.github.com>
 ;; URL: https://github.com/jcaw/theme-magic.el
 ;; Keywords: unix, faces, terminals, extensions
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Package-Requires: ((emacs "24.4") (seq "1.8"))
 
 ;; This program is free software; you can redistribute it and/or modify
