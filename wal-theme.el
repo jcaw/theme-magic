0b7c693b45d20cb32c1af198a37faca71872b740
jcaw <40725916+jcaw@users.noreply.github.com>
2019-05-06 07:51:25 -0700

    Add header
    Including copyright information

--- wal-theme.el
+++ wal-theme.el
@@ -1,4 +1,40 @@
-;; TODO: Include copyright information, copy from original spacemacs-common.el
+;;; wal-theme.el --- Automatically generate a theme from Pywal
+
+;; Copyright (C) 2015-2018 Nasser Alshammari
+;; Copyright (C) 2019 GitHub user "jcaw"
+
+;; Author: GitHub user "jcaw"
+;; Original Spacemacs Theme Author: Nasser Alshammari
+;; URL: <https://github.com/jcaw/theme-magic>
+;;
+;; Version: 0.0.1
+;; Keywords: color, theme
+;; Package-Requires: ((emacs "25"))
+
+;; Original Spacemacs themes initially created with the help of
+;; emacs-theme-generator, <https://github.com/mswift42/theme-creator>.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of Emacs.
+
+;;; Commentary:
+
+;; This is a color theme that takes 16 Pywal colors as input, and generates
+;; its look automatically.
+
+;;; Code:
 
 
 (defgroup wal-theme nil
